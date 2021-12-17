caf17f8ad64cfed262f2cef14e6eabe9f9e97ad4
Zekeriya Koc <zekzekus@gmail.com>
2021-12-18 01:36:33 +0300

    Moved project deps file to proper place

--- src/projects/deps.ts
+++
@@ -1,3 +0,0 @@
-export * as article from "../components/article/interface.ts";
-export * as database from "../components/db-access/interface.ts";
-export * as api from "../bases/rest-api/main.ts";
---
+++ src/projects/realworld-backend/deps.ts
@@ -0,0 +1,3 @@
+export * from "../../components/article/interface.ts";
+export * from "../../components/db-access/interface.ts";
+export * from "../../bases/rest-api/main.ts";
