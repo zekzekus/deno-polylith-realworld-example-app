a7948b1e57bbf34f80c00fd94eb58939aaba9bf2
Zekeriya Koc <zekzekus@gmail.com>
2021-12-18 21:07:03 +0000

    Added validation component to project deps

--- src/projects/realworld-backend/deps.ts
+++ src/projects/realworld-backend/deps.ts
@@ -1,3 +1,4 @@
 export * from "../../components/article/interface.ts";
 export * from "../../components/db-access/interface.ts";
+export * from "../../components/validation/interface.ts";
 export * from "../../bases/rest-api/main.ts";
